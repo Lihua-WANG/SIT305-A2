7b42c75997b7b3b83e8db74dcc21667ad77d5bda
wanglihua <wanglihua@deakin.edu.au>
2020-05-13 23:42:35 +1000

    Build Login page and Home page.

--- app/src/main/java/com/example/a2/MainActivity.java
+++ app/src/main/java/com/example/a2/MainActivity.java
@@ -1,14 +1,121 @@
 package com.example.a2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import java.io.IOException;
+
+import static com.example.a2.R.id.renjichoice_jiandan;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean stop = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        playbgmRing();
         setContentView(R.layout.activity_main);
+        //Assign the retrieved intent to the intentMain variable
+        Intent intentMain=getIntent();
+        //Get the username passed in Main2Activity
+        String nameInMyView=intentMain.getStringExtra("editUsername");
+        //Get man-machine battle button control by id
+        Button newGame = (Button) findViewById(R.id.new_game);
+        //Get two-player button control by id
+        Button fight = (Button) findViewById(R.id.fight);
+        //Get the control button of networked battle by id
+        Button netFight = (Button) findViewById(R.id.conn_fight);
+
+        //Register the listener for the man-machine battle button
+        newGame.setOnClickListener(new OnClickListener() {
+            Intent i ;
+            @Override
+            public void onClick(View v) {
+                // When the man-machine battle button is clicked,
+                // the AlterDialog popup window first appears
+                AlertDialog mydialog= new AlertDialog.Builder(MainActivity.this).create();
+                mydialog.show();
+                // Assign value to popup content，
+                // game_dialog is the game difficulty mode selection: simple and difficult
+                mydialog.getWindow().setContentView(R.layout.game_dialog);
+                // Register the listener for the simple mode in the pop-up window,
+                // if the user clicks the simple mode,
+                // Jump to the SingleGameActivity interface and send a flag = 1 message
+                mydialog.getWindow()
+                        .findViewById(renjichoice_jiandan)
+                        .setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                i = new Intent(MainActivity.this,SingleGameActivity.class);
+                                i.putExtra("flag",1);
+                                startActivity(i);
+                            }
+                        });
+                // Register the listener for the hard mode in the pop-up window,
+                // if the user clicks the hard mode,
+                // Jump to the SingleGameActivity interface and send a message with flag = 2
+                mydialog.getWindow()
+                        .findViewById(R.id.renjichoice_kunnan)
+                        .setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                i = new Intent(MainActivity.this,SingleGameActivity.class);
+                                //Settings appear from the right
+                                i.putExtra("flag",2);
+                                startActivity(i);
+                            }
+                        });
+            }
+        });
+
+        //Register a listener for a two-player battle button
+        // When the button is clicked, jump to the FightGameActivity.class interface
+        fight.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FightGameActivity.class));
+            }
+        });
+//        //为联网对战按钮注册监听器
+//        //当该按钮被点击时，跳转到ConnectionActivity.class界面
+//        netFight.setOnClickListener(new OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, ConnectionActivity.class));
+//            }
+//        });
+
     }
+
+    public AssetManager assetManager;
+    public MediaPlayer playbgmRing() {
+        MediaPlayer mediaPlayer = null;
+        try {
+            mediaPlayer = new MediaPlayer();
+            assetManager = getAssets();
+            AssetFileDescriptor fileDescriptor = assetManager.openFd("backgroundMusic.mp3");
+            mediaPlayer.setDataSource(fileDescriptor.getFileDescriptor(),fileDescriptor.getStartOffset(),
+                    fileDescriptor.getStartOffset());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            mediaPlayer.setLooping(true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return mediaPlayer;
+    }
+
+
 }
