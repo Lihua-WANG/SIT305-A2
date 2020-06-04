2608f39a28f5f77549cf3b9b05616e387bde0ec5
wanglihua <wanglihua@deakin.edu.au>
2020-06-05 03:04:37 +1000

    fixed one issue

--- app/src/main/java/com/example/a2/About/AboutVideoFragment.java
+++ app/src/main/java/com/example/a2/About/AboutVideoFragment.java
@@ -22,7 +22,7 @@ public class AboutVideoFragment extends Fragment {
     View view; //Define the view used to set the layout of the fragment
 
     private static final String VIDEO_TUTORIAL =
-            "https://r5---sn-ntq7yney.googlevideo.com/videoplayback?expire=1591274515&ei=s5fYXtrROIO98QOlna6YAQ&ip=103.111.29.250&id=o-AGjxIiuU5oMCzE6BE1rpwskAit0gVjLyCvNC04Tx1z7u&itag=18&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&gir=yes&clen=10483899&ratebypass=yes&dur=264.730&lmt=1563728721388818&fvip=5&c=WEB&txp=2211222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgULGujNrNQHj9Ub3uX_bVCWyc2DnqKLa47yP0fAl0jKwCIQCchSy5SypUKrng8uzypxVYUvDGh5NrnFVwNlOfYMgatg%3D%3D&rm=sn-pjvo2aopq-jb3e7l,sn-npos776&req_id=90b38b92ef2ba3ee&redirect_counter=2&cms_redirect=yes&ipbypass=yes&mh=Nr&mip=203.220.56.247&mm=29&mn=sn-ntq7yney&ms=rdu&mt=1591252834&mv=m&mvi=4&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRAIgELi5IDwJuYjjK3JZOOBJtYRVh3TPBCpO1Ma7Qm00K7QCIH71KDejGHegAxF6xKUbCZbPxjj1ecXPiO2-LRcvoDlZ";
+            "gobangrule";
 
     private VideoView mVideoView;
     private TextView mBufferingTextView;
@@ -159,13 +159,12 @@ public class AboutVideoFragment extends Fragment {
     // Get a Uri for the media sample regardless of whether that sample is
     // embedded in the app resources or available on the internet.
     private Uri getMedia(String mediaName) {
-        if (URLUtil.isValidUrl(mediaName)) {
-            // Media name is an external URL.
-            return Uri.parse(mediaName);
-        } else {
+//        if (URLUtil.isValidUrl(mediaName)) {
+//            // Media name is an external URL.
+//            return Uri.parse(mediaName);
+//        }else
             // Media name is a raw resource embedded in the app.
             return Uri.parse("android.resource://" + getActivity().getPackageName() +
                     "/raw/" + "gobangrule");
-        }
     }
 }
